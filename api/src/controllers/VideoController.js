d578944178ff5375c3c035aa6ac9bfcd00aa6126
JardanCarvalho <jardan.carvalho@gmail.com>
2022-05-07 00:42:03 -0300

    UUID lib installed

--- api/src/controllers/VideoController.js
+++ api/src/controllers/VideoController.js
@@ -1,16 +1,30 @@
-const response = require('express')
+const { response } = require('express');
+
 const Video = require('../models/Video');
 
 module.exports = {
   async index(request, response) {
     try {
       const videos = await Video.find();
-      return response.status(200).json(videos);
-    } catch (err) {
-      response.status(500).json({error: err.message})
+      return response.status(200).json({ videos });
+    } catch (err){
+      response.status(500).json({error: err.message});
     }
-  }
+  },
 
-  
+  async store(request, response) {
+    const {title, link} = request.body;
+
+    if(!title || !link){
+      return response.status(400).json( {error: 'Missing title or link.'} )
+    }
+
+    const video = new Video({
+      _id,
+      title,
+      link,
+      liked: false
+    })
+  }
 
 };
